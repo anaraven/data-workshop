779dd2b5a823406a2fb5f12322a9a488eb2a2b00
Andres Aravena <andres.aravena@gmail.com>
2015-05-12 15:31:40 +0300

    migrating to Affy schema

--- microarray.logfold.R
+++ microarray.logfold.R
@@ -1,28 +1,44 @@
 m <- read.csv("microarray.csv")
 
-lfc1 <- log2(m$X32GyHeLa.AVG_Signal)   - log2(m$KontrolHeLa.AVG_Signal)
-lfc2 <- log2(m$X32GyHeLa.AVG_Signal.1) - log2(m$KontrolHeLa.AVG_Signal.1)
-lfc3 <- log2(m$X32GyHeLa.AVG_Signal.2) - log2(m$KontrolHeLa.AVG_Signal.2)
+lfc.1 <- log2(m$T1.AVG_Signal)   - log2(m$C1.AVG_Signal)
+lfc.2 <- log2(m$T1.AVG_Signal.1) - log2(m$C1.AVG_Signal.1)
+lfc.3 <- log2(m$T1.AVG_Signal.2) - log2(m$C1.AVG_Signal.2)
 
-avg <- rep(NA, length(lfc1))
-for(i in 1:length(lfc1)) {
-  avg[i] <- mean(lfc1[i], lfc2[i], lfc3[i])
-}
+avg.1 <- (log2(m$T1.AVG_Signal)   + log2(m$C1.AVG_Signal))/2
+avg.2 <- (log2(m$T1.AVG_Signal.1) + log2(m$C1.AVG_Signal.1))/2
+avg.3 <- (log2(m$T1.AVG_Signal.2) + log2(m$C1.AVG_Signal.2))/2
+
+
+# lfc <- rep(NA, length(lfc.1))
+# for(i in 1:length(lfc.1)) {
+  # lfc[i] <- mean(lfc.1[i], lfc.2[i], lfc.3[i])
+# }
+
+plot(m$KontrolC4I.AVG_Signal,m$KontrolC4I.AVG_Signal.1,pch=".", log="xy")
+abline(0,1,col="red")
+
+lfc <- mapply(mean, lfc.1, lfc.2, lfc.3)
+avg <- mapply(mean, avg.1, avg.2, avg.3)
 
 get.p.value <- function(x,y,z) {
   return( t.test(c(x,y,z), alternative = "two.sided")$p.value )
-}
 
 p.val <- rep(NA, length(lfc1))
 for(i in 1:length(lfc1)) {
   p.val[i] <- get.p.value(lfc1[i], lfc2[i], lfc3[i])
 }
 
-avg <- mapply(mean, lfc1, lfc2, lfc3)
-p.val <- mapply(get.p.value, lfc1, lfc2, lfc3)
+p.val <- mapply(get.p.value, lfc.1, lfc.2, lfc.3)
 q.val <- p.adjust(p.val, method = "fdr")
 
-plot(avg, -log10(p.val), pch=".")
-plot(avg, -log10(p.val), pch=16, cex=0.4)
+plot(p.val, q.val, pch=16, cex=0.4); abline(0,1,col="red")
 
-r <- data.frame(PROBE_ID=m$PROBE_ID, SYMBOL= m$SYMBOL, lfc1, lfc2, lfc3, avg, p.val, q.val)
+## Adjusted Volcano Plot
+
+plot(lfc, -log10(q.val), pch=16, cex=0.4)
+abline(h=-log10(0.05), col="red")
+abline(v=1, col="red")
+abline(v=-1, col="red")
+
+# r <- data.frame(PROBE_ID=m$PROBE_ID, SYMBOL= m$SYMBOL, lfc1, lfc2, lfc3, avg, p.val, q.val)
+
